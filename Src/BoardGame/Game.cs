57ff308a4924ebd6236c9b7533059c551344324a
John Kane <John Kane>
2016-08-10 14:18:43 +0100

    Switch to calculation for lose game rule.

--- Src/BoardGame/Game.cs
+++ Src/BoardGame/Game.cs
@@ -6,10 +6,9 @@ namespace BoardGame
 {
     public class Game
     {
-        private List<string> Moves { get; set; }
         private Tuple<int, int> PlayerPosition { get; set; }
+        private List<Tuple<int, int>> PreviousPlayerPositions { get; set; }
         private IEnumerable<Tuple<int, int>> Mines { get; set; }
-        private List<Tuple<int, int>> HitMines { get; set; }
 
         public GameStatus Status { get; private set; }
 
@@ -20,9 +19,9 @@ namespace BoardGame
         public Game(IEnumerable<Tuple<int, int>> mines)
         {
             this.Status = GameStatus.Ongoing;
-            this.Moves = new List<string>();
-            this.HitMines = new List<Tuple<int, int>>();
+            
             this.PlayerPosition = Tuple.Create(0, 0);
+            this.PreviousPlayerPositions = new List<Tuple<int, int>>() { this.PlayerPosition };
 
             this.Mines = mines;
         }
@@ -58,81 +57,22 @@ namespace BoardGame
 
         public MoveResult Move(string move)
         {
-            if (move == "RIGHT")
-            {
-                if (this.PlayerPosition.Item1 == 7)
-                {
-                    return new MoveResult
-                    {
-                        Status = MoveResult.StatusFailure,
-                        Message = "Already on the rightmost column"
-                    };
-                }
+            var result = ValidMove(move);
 
-                this.PlayerPosition = Tuple.Create(this.PlayerPosition.Item1 + 1, this.PlayerPosition.Item2);
-            }
-            else if (move == "LEFT")
+            if(result != null)
             {
-                if (this.PlayerPosition.Item1 == 0)
-                {
-                    return new MoveResult
-                    {
-                        Status = MoveResult.StatusFailure,
-                        Message = "Already on the leftmost column"
-                    };
-                }
+                return result;
+            }
 
-                this.PlayerPosition = Tuple.Create(this.PlayerPosition.Item1 - 1, this.PlayerPosition.Item2);
-            }
-            else if (move == "UP")
-            {
-                if (this.PlayerPosition.Item2 == 7)
-                {
-                    return new MoveResult
-                    {
-                        Status = MoveResult.StatusFailure,
-                        Message = "Already on the top row"
-                    };
-                }
+            UpdatePosition(move);
 
-                this.PlayerPosition = Tuple.Create(this.PlayerPosition.Item1, this.PlayerPosition.Item2 + 1);
-            }
-            else if (move == "DOWN")
+            if (this.PreviousPlayerPositions.Intersect(this.Mines).Count() >= 2)
             {
-                if (this.PlayerPosition.Item2 == 0)
-                {
-                    return new MoveResult
-                    {
-                        Status = MoveResult.StatusFailure,
-                        Message = "Already on the bottom row"
-                    };
-                }
-
-                this.PlayerPosition = Tuple.Create(this.PlayerPosition.Item1, this.PlayerPosition.Item2 - 1);
-            }
-            else
-            {
+                this.Status = GameStatus.Lose;
                 return new MoveResult
                 {
-                    Status = MoveResult.StatusFailure,
-                    Message = "Unknown Move - " + move
+                    Status = MoveResult.StatusComplete
                 };
-            }
-
-            Moves.Add(move);
-
-            if (IsPlayerOnMine())
-            {
-                this.HitMines.Add(this.PlayerPosition);
-
-                if (this.HitMines.Count() >= 2)
-                {
-                    this.Status = GameStatus.Lose;
-                    return new MoveResult
-                    {
-                        Status = MoveResult.StatusComplete
-                    };
-                }
             }
 
             if (this.PlayerPosition.Item2 == 7)
@@ -144,22 +84,108 @@ namespace BoardGame
                 };
             }
 
+            this.Status = GameStatus.Ongoing;
             return new MoveResult
             {
                 Status = MoveResult.StatusSuccess
             };
         }
 
+        private MoveResult ValidMove(string move)
+        {
+            if (move == "RIGHT")
+            {
+                if (this.PlayerPosition.Item1 == 7)
+                {
+                    return new MoveResult
+                    {
+                        Status = MoveResult.StatusFailure,
+                        Message = "Already on the rightmost column"
+                    };
+                }
+            }
+            else if (move == "LEFT")
+            {
+                if (this.PlayerPosition.Item1 == 0)
+                {
+                    return new MoveResult
+                    {
+                        Status = MoveResult.StatusFailure,
+                        Message = "Already on the leftmost column"
+                    };
+                }
+            }
+            else if (move == "UP")
+            {
+                if (this.PlayerPosition.Item2 == 7)
+                {
+                    return new MoveResult
+                    {
+                        Status = MoveResult.StatusFailure,
+                        Message = "Already on the top row"
+                    };
+                }
+            }
+            else if (move == "DOWN")
+            {
+                if (this.PlayerPosition.Item2 == 0)
+                {
+                    return new MoveResult
+                    {
+                        Status = MoveResult.StatusFailure,
+                        Message = "Already on the bottom row"
+                    };
+                }
+            }
+            else
+            {
+                return new MoveResult
+                {
+                    Status = MoveResult.StatusFailure,
+                    Message = "Unknown Move - " + move
+                }; ;
+            }
+
+            return null;
+        }
+
+        private void UpdatePosition(string move)
+        {
+            if (move == "RIGHT")
+            {
+                this.PlayerPosition = Tuple.Create(this.PlayerPosition.Item1 + 1, this.PlayerPosition.Item2);
+            }
+            else if (move == "LEFT")
+            {
+                this.PlayerPosition = Tuple.Create(this.PlayerPosition.Item1 - 1, this.PlayerPosition.Item2);
+            }
+            else if (move == "UP")
+            {
+                this.PlayerPosition = Tuple.Create(this.PlayerPosition.Item1, this.PlayerPosition.Item2 + 1);
+            }
+            else if (move == "DOWN")
+            {
+                this.PlayerPosition = Tuple.Create(this.PlayerPosition.Item1, this.PlayerPosition.Item2 - 1);
+            }
+
+            this.PreviousPlayerPositions.Add(this.PlayerPosition);
+        }
+
         public override string ToString()
         {
             var board = new Board(8, 8);
 
-            foreach (var mine in this.HitMines)
+            board.SetMarker(this.PlayerPosition.Item1, this.PlayerPosition.Item2, 'X');
+
+            foreach (var mine in this.Mines)
             {
-                board.SetMarker(mine.Item1, mine.Item2, '+');
+                if(this.PreviousPlayerPositions.Contains(mine))
+                {
+                    board.SetMarker(mine.Item1, mine.Item2, '+');
+                }
             }
 
-            if (this.HitMines.Contains(this.PlayerPosition))
+            if(this.Mines.Contains(this.PlayerPosition))
             {
                 board.SetMarker(this.PlayerPosition.Item1, this.PlayerPosition.Item2, '*');
             }
