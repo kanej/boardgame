de9b71022280c86d963101933f59cbaed26715fd
John Kane <John Kane>
2016-08-10 10:39:07 +0100

    Initial setup of console app and test project.

---
+++ BoardGame/Game.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoardGame
+{
+    public class Game
+    {
+        public override string ToString()
+        {
+            return @"00000000
+00000000
+00000000
+00000000
+00000000
+00000000
+00000000
+X0000000";
+        }
+    }
+}
---
+++ BoardGameTests/GameTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BoardGame;
+
+namespace BoardGameTests
+{
+    [TestClass]
+    public class GameTests
+    {
+        [TestMethod]
+        public void a_new_game_has_the_player_in_the_bottom_left_hand_corner()
+        {
+            var game = new Game();
+
+            var expectedBoard = @"00000000
+00000000
+00000000
+00000000
+00000000
+00000000
+00000000
+X0000000";
+
+            Assert.AreEqual(expectedBoard, game.ToString());
+        }
+    }
+}
